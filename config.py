924087fa9e25e028784d0515b03f59cd6f932f17
jithusubramanian <68115980+jithusubramanian@users.noreply.github.com>
2021-01-29 16:06:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "http://streamtelegram.iamidiotareyoutoo.com/463256771376330388/2021-01-26_15:46:22.jpg")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://mydrive.curatedstuffs.workers.dev/0:/SeedBox/2021-01-29_10:33:30.jpg")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
